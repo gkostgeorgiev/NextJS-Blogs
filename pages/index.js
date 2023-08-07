79f2b084abb5d8862e1d28386923d0ae0022a309
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-07 13:36:47 +0300

    Added featured posts grid

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,45 @@
 import { Fragment } from "react";
-import Hero from '../components/home-page/hero';
+import Hero from "../components/home-page/hero";
+import FeaturedPosts from "@/components/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs1",
+    title: "Getting started with NextJS",
+    image: "getting-started-with-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2023-08-07",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with NextJS",
+    image: "getting-started-with-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2023-08-07",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with NextJS",
+    image: "getting-started-with-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2023-08-07",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with NextJS",
+    image: "getting-started-with-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2023-08-07",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
-  )
+  );
 }
 
 export default HomePage;
-
