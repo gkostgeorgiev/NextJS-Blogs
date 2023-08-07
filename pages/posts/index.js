5689b05f83eadf366512b64c69ce310a2f0153f5
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-07 13:46:01 +0300

    added all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,38 @@
+import AllPosts from "@/components/posts/all-posts";
+
+const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-nextjs1",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2023-08-07",
+    },
+    {
+      slug: "getting-started-with-nextjs2",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2023-08-07",
+    },
+    {
+      slug: "getting-started-with-nextjs3",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2023-08-07",
+    },
+    {
+      slug: "getting-started-with-nextjs4",
+      title: "Getting started with NextJS",
+      image: "getting-started-with-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2023-08-07",
+    },
+  ];
+
 function AllPostsPage() {
-  
+  return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostsPage;
