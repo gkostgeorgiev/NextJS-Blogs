da594ea29a6dfd26d174060c501c49c19f1e6f93
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-07 17:30:32 +0300

    implemented dynamic rendering of post pages and paths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,42 @@
 import PostContent from "@/components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "@/lib/posts-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  return <PostContent post={props.post}/>;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  console.log(params);
+  const { slug } = params;
+  console.log(slug);
+
+  const postData = getPostData(slug);
+  console.log(postData);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+  console.log(postFileNames);
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+  console.log(slugs);
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug: slug,
+      },
+    })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
