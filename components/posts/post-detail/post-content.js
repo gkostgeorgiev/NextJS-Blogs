86cf547b45be88af3603a7d53f360ecd28068893
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-07 14:46:04 +0300

    added several helper functions for retrieving and sorting posts from the post database in lib/posts-util

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,21 +1,22 @@
 import PostHeader from "./post-header";
-import classes from './post-content.module.css'
+import classes from "./post-content.module.css";
+import ReactMarkdown from "react-markdown";
 
 const DUMMY_POST = {
   slug: "getting-started-with-nextjs1",
   title: "Getting started with NextJS",
   image: "getting-started-with-nextjs.png",
   date: "2023-08-07",
-  content: '# This is a first post'
+  content: "# This is a first post",
 };
 
 function PostContent() {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
---
+++ posts/getting-started-with-nextjs1.md
@@ -0,0 +1,11 @@
+---
+title: 'Getting started with NextJS'
+date: '2023-08-07'
+image: 'getting-started-with-nextjs.png'
+excerpt: 'This is a dummy excerpt because I'm too lazy to make up one'
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with a [link](https://google.com)
