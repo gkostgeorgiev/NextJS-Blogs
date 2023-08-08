c751ab54c193912f885e9f07e2a2ab906a041698
Georgi Ko. Georgiev (A1 BG) <Georgi.Ko.Georgiev@a1.bg>
2023-08-08 17:31:56 +0300

    optimized post-content file for deployment

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,8 +2,15 @@ import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
+import html from 'react-syntax-highlighter/dist/cjs/languages/prism/html';
+
+SyntaxHighlighter.registerLanguage('js', js)
+SyntaxHighlighter.registerLanguage('css', css)
+SyntaxHighlighter.registerLanguage('html', html)
 
 function PostContent(props) {
   const { post } = props;
@@ -45,7 +52,7 @@ function PostContent(props) {
 
     code(code) {
       const { className, children } = code;
-      const language = className.split('-')[1];
+      const language = className.split("-")[1];
       return (
         <SyntaxHighlighter
           style={atomDark}
